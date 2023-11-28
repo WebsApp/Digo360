cad9864955d3014b4610d0d5e28bbf6d7fafcee5
Sachin <sachinsingh1880@gmail.com>
2023-11-28 18:42:01 +0530

    working on UI

---
+++ app/src/main/java/com/wapss/digo360/activity/NotificationActivity.java
@@ -0,0 +1,36 @@
+package com.wapss.digo360.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.wapss.digo360.R;
+
+public class NotificationActivity extends AppCompatActivity {
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        back = findViewById(R.id.back);
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(getWindow().getContext(), R.color.purple));
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificationActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/wapss/digo360/fragment/HomeFragment.java
+++ app/src/main/java/com/wapss/digo360/fragment/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.wapss.digo360.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
@@ -10,11 +12,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.wapss.digo360.R;
+import com.wapss.digo360.activity.NotificationActivity;
 
 import java.util.Objects;
 public class HomeFragment extends Fragment {
+    ImageView notification,help;
+    private BottomSheetDialog bottomSheetDialog;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +35,42 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View home = inflater.inflate(R.layout.fragment_home, container, false);
+        help = home.findViewById(R.id.help);
+        notification = home.findViewById(R.id.notification);
 
         Window window = requireActivity().getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(requireActivity().getWindow().getContext(), R.color.purple));
+
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog = new BottomSheetDialog(getContext(), R.style.BottomSheetTheme);
+                @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+                View view1 = LayoutInflater.from(getContext()).inflate(R.layout.help,
+                        (RelativeLayout) home.findViewById(R.id.container));
+
+                ImageView btn_cancel = view1.findViewById(R.id.btn_cancel);
+                btn_cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                bottomSheetDialog.setContentView(view1);
+                bottomSheetDialog.show();
+                bottomSheetDialog.setCanceledOnTouchOutside(false);
+            }
+        });
         return home;
     }
 }
